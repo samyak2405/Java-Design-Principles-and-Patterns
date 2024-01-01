437108f6db13fd9c974c13750ee3bb73066b81d4
Samyak moon <moonsamyak54@gmail.com>
2024-01-01 21:42:52 +0530

    Initialized all projects

---
+++ DesignPrinciples/Liskov's Substitution Principle/LiskovPrinciple/src/main/java/com/javahunter/application/LiskovPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.javahunter.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiskovPrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiskovPrincipleApplication.class, args);
+	}
+
+}
---
+++ DesignPrinciples/Open Close Principle/OpenClosedPrinciple/src/main/java/com/javahunter/application/OpenClosedPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.javahunter.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenClosedPrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenClosedPrincipleApplication.class, args);
+	}
+
+}
