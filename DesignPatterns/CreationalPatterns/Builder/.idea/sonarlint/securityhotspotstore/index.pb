8cab9ceebc6497592d7371572fefc438c10b72ec
Samyak moon <moonsamyak54@gmail.com>
2024-01-08 23:32:30 +0530

    Completed Builder Pattern

---
+++ DesignPatterns/CreationalPatterns/Builder/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+L
+BuilderPattern/src/Main.java,5/8/583d2257b06002d7d259a52bf74f457ad9610112
+P
+ BuilderPattern/src/Director.java,e/e/eeabbb8d51f39e5b1899e7387b04ae83e8ee718d
+V
+&BuilderPattern/src/StudentBuilder.java,6/a/6a320a6501ade2e1352f592af19138b6ceb0e61a
+O
+BuilderPattern/src/Student.java,f/3/f30ab86060b75de53b7c1d09d196dad2c8c59f2b
+a
+1BuilderPattern/src/EngineeringStudentBuilder.java,8/6/86ee882b0cdc0d7a35f803bb52d251893ea0b3ad
+Y
+)BuilderPattern/src/MBAStudentBuilder.java,e/b/eb480d37be6e909bf1d440f3193f158c07fdb1e7
---
+++ DesignPatterns/CreationalPatterns/Builder/BuilderPattern/src/Director.java
@@ -0,0 +1,34 @@
+public class Director {
+
+    StudentBuilder studentBuilder;
+
+    Director(StudentBuilder studentBuilder){
+        this.studentBuilder = studentBuilder;
+    }
+
+    public Student createStudent(){
+        if (studentBuilder instanceof EngineeringStudentBuilder){
+            return createEngineeringStudent();
+        }else if(studentBuilder instanceof MBAStudentBuilder){
+            return createMBAStudent();
+        }
+        return null;
+    }
+
+    private Student createMBAStudent() {
+        return studentBuilder.setRollNumber(1)
+                .setAge(22)
+                .setName("sj")
+                .setSubjects().build();
+    }
+
+    private Student createEngineeringStudent() {
+        return studentBuilder.setRollNumber(2)
+                .setAge(24)
+                .setName("sj")
+                .setFatherName("Vinod")
+                .setMotherName("Pramila")
+                .setSubjects()
+                .build();
+    }
+}
